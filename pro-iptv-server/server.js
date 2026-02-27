13f9578f8977c3eccc349840b3e4abfd7d1a960e
ankatvplus <ingilizcedoc2@gmail.com>
2026-02-27 03:25:16 +0300

    Update server.js

--- pro-iptv-server/server.js
+++ pro-iptv-server/server.js
@@ -1,35 +1,41 @@
 const express = require("express");
 const fs = require("fs");
-const { exec } = require("child_process");
+const path = require("path");
 
 const app = express();
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
-const LIST_FILE = "playlist.txt";
+const PLAYLIST_FILE = "playlist.txt";
 
+// Ana panel
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/panel.html");
+  res.sendFile(path.join(__dirname, "panel.html"));
 });
 
+// Film ekleme
 app.post("/add", (req, res) => {
-    fs.appendFileSync(LIST_FILE, req.body.url + "\n");
-    res.send("OK");
+  const url = req.body.url;
+  if (url) {
+    fs.appendFileSync(PLAYLIST_FILE, url + "\n");
+  }
+  res.redirect("/");
 });
 
+// IPTV M3U
 app.get("/playlist.m3u8", (req, res) => {
+  if (!fs.existsSync(PLAYLIST_FILE)) {
+    fs.writeFileSync(PLAYLIST_FILE, "");
+  }
+  const urls = fs.readFileSync(PLAYLIST_FILE, "utf8")
+    .split("\n")
+    .filter(Boolean);
 
-    const urls = fs.readFileSync(LIST_FILE, "utf8")
-        .split("\n")
-        .filter(Boolean);
+  const m3uContent = `#EXTM3U\n` + urls.map(u => `#EXTINF:-1,Film Kanal\n${u}`).join("\n");
 
-    const playlist = `#EXTM3U\n` +
-        urls.map(u =>
-            `#EXTINF:-1,Film\n${u}`
-        ).join("\n");
-
-    res.setHeader("Content-Type", "application/x-mpegURL");
-    res.send(playlist);
+  res.setHeader("Content-Type", "application/x-mpegURL");
+  res.send(m3uContent);
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
